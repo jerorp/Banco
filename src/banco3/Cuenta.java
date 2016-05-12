f25b573fcb190aba1ff4639f426630df6cecc8a0
Jero <jerorp@gmail.com>
2016-05-12 12:45:02 +0100

    más pruebas

--- src/banco3/Cuenta.java
+++ src/banco3/Cuenta.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-/* Este comentario es para probar GIT*/
+/* modifico el  comentario para probar GIT*/
 package banco3;
 
 import java.io.FileInputStream;
